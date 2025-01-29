317c63f14e88a29cce57051d3f2b9e1df32e5c9a
sunnnnn <putawan.sra@gmail.com>
2025-01-30 06:39:59 +0700

    add transition to LandingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ export default function LandingPage() {
   const { data:session } = useSession();
 
   const [ providers, setProviders ] = useState<ProvidersType>(null);
+  const [ isVisibleProvider, setIsVisibleProvider] = useState(false);
+  const [ isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     const setUpProviders = async () => {
       const response = await getProviders();
@@ -17,6 +19,20 @@ export default function LandingPage() {
     }
     setUpProviders();
   }, []);
+  
+  useEffect(() => {
+    setIsVisible(true);
+  }, [])
+
+  useEffect(() => {
+    if (providers) {
+      const timer = setTimeout(() => {
+        setIsVisibleProvider(true);
+      }, 300); // Delay in milliseconds, 300ms in this case
+      return () => clearTimeout(timer); // Clean up timer on unmount
+    }
+  }, [providers]);
+
 
   const handleSignIn = async (providerId: string) => {
     await signIn(providerId, {
@@ -24,28 +40,43 @@ export default function LandingPage() {
     });
   }
   return (
-    <main className="px-5 h-screen flex flex-col justify-center items-center gap-5">
+    <main className="pt-10 px-5 h-screen flex flex-col justify-center items-center gap-5">
         <header className="px-10 flex flex-col">
-          <h1 className="text-center text-5xl font-bold">
+          <h1 className={`text-center text-5xl font-bold transition-all duration-1000 ease-out transform ${isVisible ? "translate-y-0 opacity-100" : "-translate-y-16 opacity-0"}`}>
             CHIPFLOW
           </h1>
-          <h1 className="text-center mt-5 font-light">
+          <h1 className={`text-center mt-5 font-light transition-all duration-500 ease-out transform ${isVisible ? "translate-y-0 opacity-100" : "-translate-y-12 opacity-0"}`}>
             The platform for poker players to manage <span className="font-medium text-red-400">limited chips</span> with easy buying, selling, and tracking!
           </h1>
-          <div className="mt-4 flex justify-center">
+          <div className={`mt-6 flex justify-center transition-all duration-300 ease-out transform ${isVisible ? "translate-y-0 opacity-100" : "-translate-y-8 opacity-0"}`}>
             <button className="border p-3 text-sm rounded-xl">Buy me a coffee</button>
           </div>
         </header>
-        <section className="px-8 pt-6 flex justify-center items-center gap-2">
-              <Image src="/images/homepage.png" alt="HomePageImg" width={144} height={348} />
-              <div className="flex flex-col gap-12 text-2xl font-bold">
-                <h1>LIMITED CHIP</h1>
-                <h1>BUY-IN MANAGER</h1>
-                <h1>TRACKING</h1>
+        <section className="px-8 pt-4 flex justify-center items-center gap-2">
+              <Image src="/images/homepage.png" alt="HomePageImg" width={144} height={348}
+              className={`transition-all duration-500 ease-out transform ${isVisible ? "translate-x-0 opacity-100" : "-translate-x-8 opacity-0"}`}/>
+              
+              <div className={`flex flex-col gap-8 text-2xl font-bold`}>
+                <h1 className={`transition-all duration-1000 ease-out transform ${
+                    isVisible ? "translate-x-0 opacity-100 delay" : "translate-x-20 opacity-0"
+                  }`}>
+                  LIMITED CHIP
+                </h1>
+                <h1
+                  className={`transition-all duration-1000 ease-out transform ${
+                    isVisible ? "translate-x-0 opacity-100 delay-100" : "translate-x-20 opacity-0"
+                  }`}>
+                  BUY-IN MANAGER
+                </h1>
+                <h1 className={`transition-all duration-1000 ease-out transform ${
+                    isVisible ? "translate-x-0 opacity-100 delay-300" : "translate-x-20 opacity-0"
+                  }`}>
+                  TRACKING
+                </h1>
               </div>
         </section>
       {providers && Object.values(providers).map((provider) => (
-        <div key={provider.name} className="mt-4">
+        <div key={provider.name} className={`mt-4 transition-all duration-1000 ease-out ${isVisibleProvider ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'}`}>
           <button type="button" onClick={() => handleSignIn(provider.id)} className="py-4 px-14 bg-white text-black rounded-3xl transform transition-all duration-100 hover:bg-white hover:scale-105">
             <div className="flex justify-center gap-4">
               <Image src="/google.svg" alt="googleIcon" width={24} height={24}/>
