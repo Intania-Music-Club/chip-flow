a044d80b6b03cc11c049e4b27c951eeed650db63
sunnnnn <putawan.sra@gmail.com>
2025-02-05 05:04:45 +0700

    feat: add view other profile route

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+
+
+const Loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+        <div className="text-5xl font-bold">Loading ...</div>
+    </div>
+  )
+}
+
+export default Loading
